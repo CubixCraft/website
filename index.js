55d8e8a3ddd299a9744e37c93aee1c76b4e89d13
Jan Buschtöns <buschtoens@gmail.com>
2013-01-09 18:38:14 +0100

    move out up

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-// module dependencies
-var up = require("up")
-  , uphook = require("uphook")
-  , http = require("http") 
-  , app = require("./app");
-
-// redirect "cubixcraft.de" to "www.cubixcraft.de"
-http.createServer(function(req, res) {
-  res.writeHead(302, { "Location": "http://www.cubixcraft.de" + req.url });
-}).listen("/tmp/http/cubixcraft.de");
+module.exports = require("http").createServer();
